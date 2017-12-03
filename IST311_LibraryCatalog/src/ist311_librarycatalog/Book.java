183678243f62798332012b7c3b33cb9ed47e0743
eql5121 <eql5121@gmail.com>
2017-12-03 18:54:12 -0500

    PBI-6: - Sorting for book name - not implemented: book isbn, author, genre, etc.

--- IST311_LibraryCatalog/src/ist311_librarycatalog/Book.java
+++ IST311_LibraryCatalog/src/ist311_librarycatalog/Book.java
@@ -6,6 +6,11 @@
  */
 package ist311_librarycatalog;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * This class will contain fields and methods for book objects that will go in
  * the catalog.
@@ -13,7 +18,7 @@ package ist311_librarycatalog;
  * @author Eric Liang <eql5121@psu.edu>
  * @author Jennifer A'Harrah <jka5240@psu.edu>
  */
-public class Book {
+public class Book implements Comparable<Book> {
 
     // Declare private fields needed when creating a book object
     private String _ISBN;
@@ -23,6 +28,7 @@ public class Book {
     private String _genre2;
     private String _genre3;
     private boolean _borrowed;
+    private int _numOfBooks;
 
     // Constructors for easily creating new book objects
     // Constructor for book object with one genre
@@ -58,7 +64,6 @@ public class Book {
     /*
      ===========GETTERS AND SETTERS==================
      */
-
     /**
      *
      * @return the private ISBN string
@@ -154,9 +159,16 @@ public class Book {
     public void setGenre3(String genre3) {
         _genre3 = genre3;
     }
-    
+
+    public int getNumOfBooks() {
+        return _numOfBooks;
+    }
+
+    public void setNumOfBooks(int _numOfBooks) {
+        this._numOfBooks = _numOfBooks;
+    }
+
     //=================METHODS==============
-    
     /**
      * Will mark a book as borrowed
      */
@@ -170,9 +182,10 @@ public class Book {
     public void returned() {
         this._borrowed = false;
     }
-    
+
     /**
      * Method to check if a book has been borrowed
+     *
      * @return the string message that displays whether the book is available
      */
     public String isBorrowed() {
@@ -183,8 +196,50 @@ public class Book {
         } else if (this._borrowed == false) {
             availability = "This book is available!";
         }
-        
+
         return availability;
     }
+    // ======= Sorting methods =========
+    @Override
+    public int compareTo(Book compareBook) {
+        int compareNumbers = ((Book) compareBook).getNumOfBooks();
 
+        //ascending order
+        return this._numOfBooks - compareNumbers;
+
+        //descending order
+        //return comapreNumbers - this._numOfBooks;
+    }
+
+    public static Comparator<Book> BookNameComparator = new Comparator<Book>() {
+        public int compare(Book book1, Book book2) {
+            String bookName1 = book1.getTitle().toUpperCase();
+            String bookName2 = book2.getTitle().toUpperCase();
+
+            //ascending order
+            return bookName1.compareTo(bookName2);
+
+            //descending order
+            // bookName2.compareTo(bookName1);
+        }
+    };
+    // PBI-6: 
+    // Sorting for book name
+    // not implemented: book isbn, author, genre, etc.
+    public static void main(String[] args) {
+        Catalog catalog = new Catalog();
+        List<Book> books = new ArrayList<>(catalog.getBooks());
+        
+        
+        for (Book book : books) {
+            System.out.println(book.getTitle());
+        }
+
+        books.sort(BookNameComparator);
+        System.out.println("====After====");
+        for (Book book : books) {
+            System.out.println(book.getTitle());
+        }
+
+    }
 }
